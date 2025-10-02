f334ee9d392af93acfbc0f99f58080f8f1baed14
Jinling Li <j2897li@uwaterloo.ca>
2025-10-01 21:43:35 -0400

    initial commit from tutorial

---
+++ task-manager/src/App.jsx
@@ -0,0 +1,45 @@
+import { use, useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const Card = ({title}) => {
+  const [hasLiked, setHasLiked] = useState(false);
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    console.log(`Card ${title} has been ${hasLiked ? "liked" : "unliked"}`);
+  }, [hasLiked]);
+
+
+  return (
+    <div className = "card" onClick={()=>setCount((prevState)=>prevState+1)}>
+      <h2>{title} <br/> {count}</h2>
+
+      <button onClick={() => setHasLiked((prevState)=>(!prevState))}>
+        {hasLiked ? "💗" : "🤍"}
+        </button>
+    </div>
+  )
+}
+
+
+const App = () => {
+
+
+  return (
+  <div className = "card-container">
+    
+    <Card title = "star"/>
+    <Card title = "ava"/>
+    <Card title = "eef"/>
+
+
+  </div>
+  )
+}
+
+
+
+
+export default App
