ac6b2dc080d7eb5987a6dcd4f893ca637d737b3b
Jinling Li <ljling0314@gmail.com>
2025-10-10 15:50:56 -0700

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
   const [movies, setMovies] = useState([]);   
   const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const fetchMovies = async () => {
     const term = searchTerm.trim();
@@ -22,7 +23,8 @@ const App = () => {
       setErrorMessage(null);
       return;
     }
-
+    setLoading(true);
+    setErrorMessage(null);
     try {
       setErrorMessage(null);
 
@@ -38,12 +40,19 @@ const App = () => {
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
 
       const data = await res.json();
+      if(data.res === 'False') {
+        setMovies([]);
+        setErrorMessage(data.error || "No movies found.");
+        return;
+      }
       console.log("Result count:", data.resultCount); 
       setMovies(data.results || []);
     } catch (err) {
       console.error("Error fetching movies:", err);
       setMovies([]);
       setErrorMessage("Failed to fetch movies. Please try again later.");
+    }finally{
+      setLoading(false);
     }
   };
   
@@ -81,15 +90,14 @@ const App = () => {
 
         <section className = "all-movies">
           <h2>All Movies</h2>
-
-          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-          <ul className="mt-3 grid gap-2">
+          {loading ? (<p>Loading...</p>):errorMessage ? (<p className="text-red-500">{errorMessage}</p>) :
+          (<ul className="mt-3 grid gap-2">
             {movies.map((m) => (
               <li key={m.trackId} className="border rounded p-2">
                 {m.trackName}
               </li>
             ))}
-          </ul>
+          </ul>)}
         </section>
 
         
